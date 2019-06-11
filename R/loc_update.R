6cbbe90ddb383123951b49f7379f0d592b234409
andrewpita <adrewpita@gmail.com>
2019-06-11 13:25:05 -0400

    modified loc_update so that it can run locations with only two listings

--- R/loc_update.R
+++ R/loc_update.R
@@ -18,6 +18,7 @@ loc_update = function(locList, a.phi, b.phi) {
   #grab the number of listings including survey
   nListings = nrow(locList[[1]])
   
+  
   ##### calculate x for phi and N update
   #x = phi.location * (1 - inclusion prob)
   x = locList[[6]] * (1 - locList[[1]][1,3])
@@ -30,30 +31,46 @@ loc_update = function(locList, a.phi, b.phi) {
   
   ######
   
-  A = locList[[1]][2,1] #source beside survey
-  B = locList[[1]][3,1] #other source beside survey
-  C = locList[[1]][1,1] #the survey
-  ABC = locList[[1]][1,2] #all three sources
-  BC = locList[[1]][3,2] #B and C overlap
-  AC = locList[[1]][2,2] #A and C overlap
+  ###### calculate r for N update
   
-  nA.BC = BC - ABC
-  nA = locList[[2]] - A
+  if (nListings == 2) {
+    
+    A = locList[[1]][2,1] #source beside survey
+    B = locList[[1]][1,1] # survey
+    AB = locList[[1]][2,2] #overlap between survey and other source
+    
+    r = A + B - AB
+    
+  } else if (nListings == 3) {
+    
+    A = locList[[1]][2,1] #source beside survey
+    B = locList[[1]][3,1] #other source beside survey
+    C = locList[[1]][1,1] #the survey
+    ABC = locList[[1]][1,2] #all three sources
+    BC = locList[[1]][3,2] #B and C overlap
+    AC = locList[[1]][2,2] #A and C overlap
+    
+    nA.BC = BC - ABC
+    nA = locList[[2]] - A
+    
+    #### calculate r for N update
+    
+    #vector containing AB, not A B = c(ABC, notA BC) + 1 draw from multivariate hyper of
+    #B - (ABC + notA BC balls), with A - ABC balls of one color, and not A - ABC balls of another
+    AB.nAB.vec = c(ABC, nA.BC) + 
+      rmvhyper(1, 
+               k = B - ABC - nA.BC,
+               n = c(A - ABC, 
+                     nA - ABC))
+    
+    #r = A + B + C - AC - BC - (AB - ABC)
+    r = A + B + C - AC - BC - (AB.nAB.vec[1] - ABC)
+    
+    #####
+    
+    
+  }
   
-  #### calculate r for N update
-  
-  #vector containing AB, not A B = c(ABC, notA BC) + 1 draw from multivariate hyper of
-  #B - (ABC + notA BC balls), with A - ABC balls of one color, and not A - ABC balls of another
-  AB.nAB.vec = c(ABC, nA.BC) + 
-    rmvhyper(1, 
-             k = B - ABC - nA.BC,
-             n = c(A - ABC, 
-                   nA - ABC))
-  
-  #r = A + B + C - AC - BC - (AB - ABC)
-  r = A + B + C - AC - BC - (AB.nAB.vec[1] - ABC)
-  
-  #####
   
   #### update N
   locList[[2]] = r + rbinom(1, locList[[3]] - r, x/(1 - locList[[6]] + x))
